ee87961b960060b0a2fb37987f1b7ade9ff0640a
Wallace Palmer <darthdegen@gmail.com>
2026-02-06 11:00:49 -0700

    feat: add user profile

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
-export default function Home() {
+import { UserProfile } from "@/components/profile/UserProfile";
+import { getUser } from "@/lib/api";
+import { Separator } from "@/components/ui/separator";
+
+export default async function Home() {
+  const user = await getUser();
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-black">
-        {/*  */}
+    <div className="flex min-h-screen items-center justify-center bg-background">
+      <main className="flex min-h-screen w-full max-w-4xl xl:max-w-5xl flex-col items-center justify-start py-32 px-16 bg-background">
+        <UserProfile user={user} />
+        <Separator className="my-10" />
       </main>
     </div>
   );
---
+++ components/profile/UserProfile.tsx
@@ -0,0 +1,30 @@
+import { User } from "@/types/User";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+
+type UserProfileProps = {
+  user: User;
+};
+
+export function UserProfile({ user }: UserProfileProps) {
+  return (
+    <section className="flex flex-col gap-3 w-full ">
+      <Avatar size="lg">
+        <AvatarImage src={`/images/${user.avatarUrl}`} />
+        <AvatarFallback>{user.name[0]}</AvatarFallback>
+      </Avatar>
+
+      <div>
+        <p className="text-xl text-white/90 font-bold">{user.name}</p>
+        <p className="text-sm text-white/50">@{user.username}</p>
+      </div>
+
+      <p className="text-lg text-white/70">{user.bio}</p>
+
+      <div className="flex gap-2">
+        {user.travelStatus && <Badge> {user.travelStatus}</Badge>}
+        {user.location && <Badge variant="secondary"> {user.location}</Badge>}
+      </div>
+    </section>
+  );
+}
